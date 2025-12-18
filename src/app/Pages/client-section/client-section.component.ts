f5d6d32e930bc46a714b0c6f777309d0392c766d
Kido-kid <akshaygit.git@gmail.com>
2025-12-18 10:07:36 +0530

    Added Client Sectin

---
+++ src/app/Pages/client-section/client-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-section',
+  imports: [],
+  templateUrl: './client-section.component.html',
+  styleUrl: './client-section.component.scss'
+})
+export class ClientSectionComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroSectionComponent } from './Pages/hero-section/hero-section.component';
+import { ClientSectionComponent } from './Pages/client-section/client-section.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ClientSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
