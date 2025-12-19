4d697a9bf2cf32e704a7eb73371959e8e9677b52
Kido-kid <akshaygit.git@gmail.com>
2025-12-19 18:43:42 +0530

    Third Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,16 @@ import { AchievementsComponent } from './Pages/achievements/achievements.compone
 import { ArticlePreviewComponent } from './Pages/article-preview/article-preview.component';
 import { TestimonialComponent } from './Pages/testimonial/testimonial.component';
 import { DemoNavigationComponent } from './Pages/demo-navigation/demo-navigation.component';
+import { CommunityUpdatesComponent } from './Pages/community-updates/community-updates.component';
+import { HeaderComponent } from './Pages/header/header.component';
+import { FooterComponent } from './Pages/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ClientSectionComponent,ValuePropositionComponent,PixelgradeComponent,AchievementsComponent,ArticlePreviewComponent,TestimonialComponent,DemoNavigationComponent],
+  imports: [RouterOutlet,ClientSectionComponent,ValuePropositionComponent,
+    PixelgradeComponent,AchievementsComponent,ArticlePreviewComponent,
+    TestimonialComponent,DemoNavigationComponent,CommunityUpdatesComponent,
+    HeaderComponent,HeroSectionComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
